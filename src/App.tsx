1e7eb9b520147445ff5296fc5e4d0d2e4ed79fd0
senkentarou <masahiro-senda@c-fo.com>
2022-01-30 00:49:40 +0900

    雛形作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { makeStyles, AppBar, Container, Card, CardContent, Typography } from '@material-ui/core';
+import SearchBar from 'material-ui-search-bar';
+
+const useStyles = makeStyles({
+  appBar: {
+    width: '100%',
+    height: '5rem',
+    marginBottom: '1rem'
+  },
+  title: {
+    marginTop: '1rem',
+    marginLeft: '1rem'
+  },
+  mainCard: {
+    width: '100%',
+    height: '100%'
+  },
+  searchBar: {
+    marginTop: '1rem',
+    maxWidth: '800px'
+  }
+});
+
+export const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar position="static" className={classes.appBar}>
+        <Typography variant="h3" component="h1" className={classes.title}>
+          Sample kindle unlimited 検索アプリ
+        </Typography>
+      </AppBar>
+      <Container maxWidth={false}>
+        <Card className={classes.mainCard}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              検索しよう
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              kindle unlimited の検索結果を表示します
+            </Typography>
+            <SearchBar className={classes.searchBar} />
+          </CardContent>
+        </Card>
+      </Container>
+    </>
   );
-}
-
-export default App;
+};
