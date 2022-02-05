539222688455d3c353555eb5bb191e052111c599
senkentarou <masahiro-senda@c-fo.com>
2022-02-06 01:02:36 +0900

    :sparkles: add search option selection on checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useCallback } from 'react';
 
-import { styled, AppBar, Container, Card, CardContent, Typography } from '@material-ui/core';
+import { styled, AppBar, Container, Card, CardContent, Typography, FormControl, FormGroup, FormControlLabel, Checkbox, FormHelperText } from '@material-ui/core';
 import SearchBar from 'material-ui-search-bar';
 
 const KindleUnlimitedSearchBar = styled(SearchBar)({
@@ -24,16 +24,44 @@ const SearchCard = styled(Card)({
   height: '100%'
 });
 
+const KINDLE_SEARCH_OPTION_MAP = {
+  kindleUnlimited: 'p_n_feature_nineteen_browse-bin:3169286051',
+  primeReading: 'p_n_special_merchandising_browse-bin:5304495051'
+};
+
+type KindleSearchOption = 'kindleUnlimited' | 'primeReading';
+type SearchOption = {[keys in KindleSearchOption]: boolean};
+
 export const App: React.FC = () => {
   const [searchWord, setSearchWord] = useState<string>('');
+  const [searchOptions, setSearchOptions] = useState<SearchOption>({
+    kindleUnlimited: true,
+    primeReading: true
+  });
 
   const search = useCallback(() => {
+    const initialOptions = ['n:2250738051'];
+    const selectedOptions = (Object.keys(searchOptions) as Array<KindleSearchOption>).reduce((accArray, key) => {
+      if (searchOptions[key]) {
+        accArray.push(KINDLE_SEARCH_OPTION_MAP[key]);
+      }
+
+      return accArray;
+    }, initialOptions);
+
     window.open(
-      `https://www.amazon.co.jp/s?k=${encodeURI(searchWord)}&rh=n%3A2250738051%2Cp_n_feature_nineteen_browse-bin%3A3169286051`,
+      `https://www.amazon.co.jp/s?k=${encodeURIComponent(searchWord)}&rh=${encodeURIComponent(selectedOptions.join(','))}`,
       '_blank',
       'noopener noreferrer'
     )
-  }, [searchWord]);
+  }, [searchWord, searchOptions]);
+
+  const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchOptions({
+      ...searchOptions,
+      [event.currentTarget.name]: event.currentTarget.checked
+    })
+  }, [searchOptions, setSearchOptions]);
 
   return (
     <>
@@ -51,6 +79,23 @@ export const App: React.FC = () => {
             <Typography variant="body2" color="textSecondary" component="p">
               入力した文字でAmazon webサイトのkindle検索結果を開きます（別タブ）
             </Typography>
+            <FormControl component="fieldset" variant="standard">
+              <FormGroup row={true}>
+                <FormControlLabel
+                  control={
+                    <Checkbox checked={searchOptions.kindleUnlimited} onChange={handleChange} name="kindleUnlimited" />
+                  }
+                  label="kindle unlimited"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox checked={searchOptions.primeReading} onChange={handleChange} name="primeReading" />
+                  }
+                  label="prime reading"
+                />
+              </FormGroup>
+              <FormHelperText>チェックが付いた項目が検索オプションに含まれます</FormHelperText>
+            </FormControl>
             <KindleUnlimitedSearchBar
               value={searchWord}
               onChange={(word: string) => setSearchWord(word)}
